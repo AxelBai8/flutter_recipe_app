b7c039d0f14e89ddae72ea76d4d7651fc8e8fe9d
AxelBai8 <axxelb14@gmail.com>
2025-07-23 13:34:02 -0600

    added list event

---
+++ lib/features/recipes/presentation/bloc/recipe_list_event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+abstract class RecipeListEvent extends Equatable {
+  const RecipeListEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetInitialRecipes extends RecipeListEvent {}
